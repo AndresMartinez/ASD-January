c6215df0aa61f9e0ab03b735c01876e522628e4c
Andres Martinez <Indosmoke310@gmail.com>
2012-01-04 13:20:12 -0800

    Getting started

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var forIform = function(data){
+   alert("here is your data "+data);
+};
+$(document).ready(function(){
+    var iform=$('#Form'),
+        fperrorslink = $('#fperrorslink');
+    
+    iform.validate({
+        invalidHandler: function(form, validator){
+            fperrorslink.click();
+            var html = '';
+            for (var key in validator.submitted){
+                var label=$('label[for^="'+key+'"]').not('[generated]');
+                var legend=label.closest('fieldset').find('.ui-controlgroup-label');
+                var fieldname = legend.length ? legend.text() : label.text();
+                html += '<li>' + fieldname +'</li>';
+            };
+            $("#formpageerrors ul").html(html);
+        },
+        submitHandler: function(){
+            var data = iform.serializeArray();
+            alert("form submitted");
+            forIForm(data);
+        }
+    });
+});
+
+
+
+
+var item = {
+    "title"   : "Undead Monkey",
+    "species" : "Capuchin",
+    "name"    : "Jack",
+    "priceUSD": 19.95
+};
+var serializedItem = JSON.stringify(item);
+console.log(serializedItem);
+
+
+
+
+
+
+
+
+
+
+
+
